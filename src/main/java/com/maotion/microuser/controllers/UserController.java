0887bf57c7a726152a37237757403d6481dd935d
mao <maoyuan@gmail.com>
2020-04-04 23:01:10 -0400

    cloud bus

--- src/main/java/com/maotion/microuser/controllers/UserController.java
+++ src/main/java/com/maotion/microuser/controllers/UserController.java
@@ -2,23 +2,29 @@ package com.maotion.microuser.controllers;
 
 import com.maotion.microuser.entities.UserAccount;
 import com.maotion.microuser.services.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    private Logger logger = LogManager.getLogger(UserController.class);
+    Environment environment;
     private UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService,Environment environment) {
         this.userService = userService;
+        this.environment = environment;
     }
 
     @PostMapping("/user")
     public UserAccount setupUser(@RequestBody UserAccount userAccount) {
+        logger.debug(String.format("--------the bus.refresh.test value is : %s --------",environment.getProperty("bus.refresh.test")));
         return userService.save(userAccount);
     }
 }
